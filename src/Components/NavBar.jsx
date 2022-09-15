cd7fa49d6cdda0145a0476878ebe315e9880bc9d
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-09-15 16:22:34 -0300

    preparando para estilizar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,8 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {useState,useEffect} from 'react'
-//import Git from '../svg/Git'
-//import Linkedin from '../svg/Linkedin';
+import Logo from '../assets/img/logo2.png'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon4 from '../assets/img/nav-icon1.svg'
+
 
 function NavBar() {
     const [activeLink, setActiveLink] = useState('home')
@@ -26,11 +28,11 @@ function NavBar() {
     function onUpdateActiveLink(linkName){
         setActiveLink(linkName)
     }
-    
+
   return (
     <Navbar bg="light" expand="lg" className={scroled ? "scrolled": ""}>
       <Container>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Brand href="#home"><img width="100px" height="60px" src={Logo}></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -39,8 +41,8 @@ function NavBar() {
             <Nav.Link href="#projects"  className={activeLink === "projects" ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink("projects")} >Projetos</Nav.Link>
             <span className="navbar-text">
                 <div className="social-icon">
-                    <a href='#'></a>
-                    <a href='#'></a>
+                    <a href='#'><img src={navIcon1}></img></a>
+                    <a href='#'><img  src={navIcon4}></img></a>
                 </div>
                 <button className="btn-contact" onClick={()=> console.log("clicado")}>
                 Contato
