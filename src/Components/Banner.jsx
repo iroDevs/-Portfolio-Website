a7efc84e9d25a027965103f7cbb28259652c143c
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-09-17 00:19:59 -0300

    logica das letras aparecendo

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Zap from '../assets/img/whatsIcon.svg'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
+import FloatImage from "../assets/img/header-img.svg"
 
 export default function Banner() {
+    const [loopNum,setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false)
+    const toRotate = ["Desenvolvedor Back-End","Desenvolvedor Front-End","Desenvolvedor FullStack"]
+    const [text , setText] = useState("");
+    //tempo que as letras vão demorar a mudar
+    const [delta,setDelta] = useState(300 - Math.random() * 100)
+    const period = 2000;
   return (
     <section className="banner" id="home">
         <Container>
@@ -15,7 +23,7 @@ export default function Banner() {
                     <button onClick={()=> console.log('Conectando')}>Me mande uma messagem <AiOutlineWhatsApp size={24}></AiOutlineWhatsApp></button>
                 </Col>
                 <Col xs={12} md={6} xl={7}>
-                    <img src={"aa"} alt="Background"></img>
+                    <img src={FloatImage} alt="Background"></img>
                 </Col>
             </Row>
         </Container>
