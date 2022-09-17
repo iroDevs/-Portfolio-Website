ab4792052cf9455f67643d8f32082c236788c8ea
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2022-09-17 00:32:21 -0300

    atualizando funçao que escreve nomes

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,9 +9,43 @@ export default function Banner() {
     const [isDeleting, setIsDeleting] = useState(false)
     const toRotate = ["Desenvolvedor Back-End","Desenvolvedor Front-End","Desenvolvedor FullStack"]
     const [text , setText] = useState("");
+    const [index, setIndex] = useState(1);
     //tempo que as letras vão demorar a mudar
     const [delta,setDelta] = useState(300 - Math.random() * 100)
     const period = 2000;
+
+    useEffect(()=> {
+        let ticker = setInterval(()=>{
+            tick();
+        },delta)
+        return() => { clearInterval(ticker) }
+    },[text])
+
+    function tick() {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(500);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
   return (
     <section className="banner" id="home">
         <Container>
