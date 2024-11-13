d205e08069efc3d5e90a404c23461e8a8b29b580
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2024-11-13 19:02:30 -0300

    Commit

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -6,6 +6,11 @@ import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
+import CardProject from "./sub-components/CardProject";
+import projeto1 from '../assets/projetos/projeto1.png'
+import projeto2 from '../assets/projetos/projeto2.png'
+import projeto3 from '../assets/projetos/projeto3.png'
+import projeto4 from '../assets/projetos/projeto4.png'
 
 export default function Skills() {
   const responsive = {
@@ -28,15 +33,24 @@ export default function Skills() {
     }
   };
 
+  const texto = ""
+  const url = "https://www.next4.com.br/wp-content/themes/yootheme/cache/f2/next4-blog-site-profissional-f2be572e.png"
+  const title = "Projeto 1"
+
   return (
     <section className="projects" id="projetos">
         <div className="">
             <div className="">
                 <div className="col-12">
                     <div className="projects-bx wow zoomIn">
-                      <h4>Projetos</h4>
-                      <div>
-
+                      <h2>Projetos</h2>
+                      <div className="cards-list">
+                        <CardProject text={texto} title={'Portal Cosems'} img={projeto1} url={'https://portal.cosemsmg.org.br/login'} />
+                        <CardProject text={texto} title={'Drumundo&Crivellari Advogados'} img={projeto2} url={'https://drumondcrivellari.adv.br/'} />
+                      </div>
+                      <div className="cards-list">
+                        <CardProject text={texto} title={'Site Cosems'} img={projeto3} url={'https://www.cosemsmg.org.br/'} />
+                        <CardProject text={texto} title={'SOST'} img={projeto4} url={'#'} />
                       </div>
 
                     </div>
